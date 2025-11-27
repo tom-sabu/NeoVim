c98907b7ca72646c463d79259de3b3ff43d89dac
Tom <tomsabu@tutamail.com>
2025-11-27 11:24:31 +0530

    new config: none_ls, alpha

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,24 +1,23 @@
-return
-{
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "MunifTanjim/nui.nvim",
-            "nvim-tree/nvim-web-devicons",
-        },
-        config = function()
-            require("neo-tree").setup({
-                filesystem = {
-                    follow_current_file = {
-                        enabled = true,
-                    },
-                },
-            })
+return {
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("neo-tree").setup({
+				filesystem = {
+					follow_current_file = {
+						enabled = true,
+					},
+				},
+			})
 
-            vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', { desc = "Toggle Neo-tree" })
-            vim.keymap.set('n', '<C-o>', ':Neotree close<CR>', { desc = "Close Neo-tree" })
-        end,
-    }
+			vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", { desc = "Toggle Neo-tree" })
+			vim.keymap.set("n", "<C-o>", ":Neotree close<CR>", { desc = "Close Neo-tree" })
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,24 +1,24 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "lua",
-                    "vim",
-                    "vimdoc",
-                    "python",
-                    "c",
-                    "cpp"
-                },
-                highlight = {
-                    enable = true,
-                },
-                indent = {
-                    enable = true,
-                },
-            })
-        end,
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"lua",
+					"vim",
+					"vimdoc",
+					"python",
+					"c",
+					"cpp",
+				},
+				highlight = {
+					enable = true,
+				},
+				indent = {
+					enable = true,
+				},
+			})
+		end,
+	},
 }
