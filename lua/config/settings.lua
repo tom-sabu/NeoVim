c24a52b71ca05c9f12b3f9a73f1d9604380922ac
Tom <tomsabu@tutamail.com>
2025-12-27 13:39:16 +0530

    added new plugins.

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -19,9 +19,3 @@ vim.keymap.set('n', '<leader>tt', ':terminal<CR>', { desc = 'Terminal in current
 
 -- escape terminal mode easily
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
--- navigate between splits in terminal mode
-vim.keymap.set('t', '<C-h>', '<C-\\><C-n><C-w>h', { desc = 'Move to left split' })
-vim.keymap.set('t', '<C-j>', '<C-\\><C-n><C-w>j', { desc = 'Move to bottom split' })
-vim.keymap.set('t', '<C-k>', '<C-\\><C-n><C-w>k', { desc = 'Move to top split' })
-vim.keymap.set('t', '<C-l>', '<C-\\><C-n><C-w>l', { desc = 'Move to right split' })
---
+++ lua/plugins/indent.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		config = function()
+			require("ibl").setup({
+				scope = {
+					enabled = true,
+					show_start = true,
+					show_end = true,
+				},
+			})
+		end,
+	},
+}
